91eb51f5584b07b55597678a21d0457364f5e4c6
Erik Sundvall <erik.sundvall@regionstockholm.se>
2025-07-29 11:39:26 +0200

    fix: update variable names for clarity and improve output data handling in SvelteWidget

--- src/SvelteWidget.svelte
+++ src/SvelteWidget.svelte
@@ -26,35 +26,35 @@
             }
         }
     };
-    let widgetData
+    let outputData
     let hostElement;
 
-    $: widgetData = useDefaultData !== false ? "This is a default value" : inputData
+    $: outputData = useDefaultData !== false ? "This is a default value" : inputData
 
     onMount(() => {
         // When the component is first mounted, emit the initial data.
         // This is done in onMount to ensure the hostElement is bound and
         // the event listener on the host page has had a chance to be attached.
-        emitWidgetData();
+        emitOutputData();
     });
 
-    function emitWidgetData() {
+    function emitOutputData() {
         if (!hostElement) return;
         // Dispatch a standard DOM CustomEvent that can bubble up and cross the shadow DOM boundary.
         hostElement.dispatchEvent(new CustomEvent('message', {
-            detail: { widgetData },
+            detail: { outputData: outputData },
             bubbles: true,
             composed: true,
         }));
     }
 
     function reverseAndEmit() {
-        // Ensure widgetData is treated as a string for reversal
-        const stringValue = String(widgetData);
-        // Reverse the string and update widgetData.
-        widgetData = stringValue.split('').reverse().join('');
+        // Ensure outputData is treated as a string for reversal
+        const stringValue = String(outputData);
+        // Reverse the string and update outputData.
+        outputData = stringValue.split('').reverse().join('');
         // Explicitly emit the change
-        emitWidgetData();
+        emitOutputData();
     }
 
 </script>
@@ -62,10 +62,10 @@
 <div bind:this={hostElement}>
 
 This is a widget example. <br>
-here you see basic input that also outputs data:<br>
-<input bind:value={widgetData}> <br>
+Here you see input data that is also used as output data:<br>
+<input bind:value={outputData}> <br>
 
-<button on:click={emitWidgetData}>Emit data</button>
+<button on:click={emitOutputData}>Emit data</button>
 <button on:click={reverseAndEmit}>Reverse and then emit data</button>
 
 {#if config && config.showButton === true}
@@ -75,14 +75,12 @@ here you see basic input that also outputs data:<br>
 {#if config && config.showDebug === true}
 <h1>Debug information (inside widget)</h1>
 <ul>
-    <li>widget data: {widgetData}</li>
-    <li>input data: {inputData}</li>
     <li>use default data: {useDefaultData}</li>
-    <li>widgetData as JSON: {JSON.stringify(widgetData)}</li>
-    <li>inputData as JSON: {JSON.stringify(inputData)}</li>
-    <li>useDefaultData as JSON: {JSON.stringify(useDefaultData)}</li>
+    <li>outputData as JSON: {JSON.stringify(outputData)}</li>
+    <li>inputData as JSON: {JSON.stringify(inputData, null, 2)}</li>
     <li>config as JSON: <br><pre>{JSON.stringify(config, null, 2)}</pre></li>
     <li>descriptor as JSON: <br><pre>{JSON.stringify(descriptor, null, 2)}</pre></li>
 </ul>
 {/if}
 </div>
+
